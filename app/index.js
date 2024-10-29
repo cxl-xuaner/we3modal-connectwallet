97e3a022ad5dfda5bc6d3197a494241a825fa168
cxl_xuaner <976883555@qq.com>
2024-10-29 18:03:06 +0000

    update index

---
+++ app/index.js
@@ -0,0 +1,15 @@
+// app/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>欢迎来到我的应用!</h1>
+      <p>访问 <a href="/newblockinfo">新区块信息</a></p>
+      <p>访问 <a href="/">钱包连接</a></p>
+
+    </div>
+  );
+};
+
+export default HomePage;
